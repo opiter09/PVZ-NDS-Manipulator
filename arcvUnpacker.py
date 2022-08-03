eac3190f9dea3156351650c0bbecfccef15fbd83
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-08-03 11:46:16 -0400

    skipping things

--- arcvUnpacker.py
+++ arcvUnpacker.py
@@ -35,6 +35,7 @@ if (len(sys.argv) > 2):
     exit()
 
 numberOfFiles = int.from_bytes(bytE[4:8], "little")
+skip = 0
 
 for i in range(numberOfFiles):
     offset = int.from_bytes(bytE[(16 + (i * 8)):(16 + (i * 8) + 4)], "little")
@@ -61,7 +62,44 @@ for i in range(numberOfFiles):
     elif (int.from_bytes(data[0:4], "big") == 1381254734):
         extension = ".NFTR"
 
-    if (size < 10000000) and ((offset + size) < len(bytE)):
-        newFile = open(directory + "/" + str(offset) + "_" + str(i).zfill(4) + extension, "wb")
+    if (size < 10000000) and (offset > 0) and ((offset + size) < len(bytE)):
+        newFile = open(directory + "/" + str(offset) + "_" + str(i + 1).zfill(4) + extension, "wb")
+        newFile.write(data)
+        newFile.close()
+    else:
+        skip = skip + 1
+
+if (skip == 0):
+    exit()
+
+for i in range(skip):
+    j = i + 1 + numberOfFiles
+    
+    offset = int.from_bytes(bytE[(16 + (j * 8)):(16 + (j * 8) + 4)], "little")
+    size = int.from_bytes(bytE[(16 + (j * 8) + 4):(16 + (j * 8) + 8)], "little")
+    data = bytE[offset:(offset + size)]
+    
+    extension = ".bin"
+    if (int.from_bytes(data[0:4], "big") == 1380401998):
+        extension = ".NCGR"
+    elif (int.from_bytes(data[0:4], "big") == 1380074318):
+        extension = ".NCBR"
+    elif (int.from_bytes(data[0:4], "big") == 1380729678):
+        extension = ".NCLR"
+    elif (int.from_bytes(data[0:4], "big") == 1380860238):
+        extension = ".NANR"
+    elif (int.from_bytes(data[0:4], "big") == 1380270926):
+        extension = ".NCER"
+    elif (int.from_bytes(data[0:4], "big") == 1380143950):
+        extension = ".NSCR"
+    elif (int.from_bytes(data[0:4], "big") == 1380011342):
+        extension = ".NMAR"
+    elif (int.from_bytes(data[0:4], "big") == 1380142414):
+        extension = ".NMCR"
+    elif (int.from_bytes(data[0:4], "big") == 1381254734):
+        extension = ".NFTR"
+
+    if (size < 10000000) and (offset > 0) and ((offset + size) < len(bytE)):
+        newFile = open(directory + "/" + str(offset) + "_" + str(j + 1).zfill(4) + extension, "wb")
         newFile.write(data)
         newFile.close()
